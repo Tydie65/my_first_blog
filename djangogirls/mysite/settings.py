5ea88d37de297f89dbd8c6c71f6d9e11ea1629aa
Tydie65 <99049773+Tydie65@users.noreply.github.com>
2023-10-03 19:53:21 -0500

    Update settings.py

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#tfqx!ex5yul)c-a3_0=!%i4o$&-xk(77m%6q$npitzs28^55!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
